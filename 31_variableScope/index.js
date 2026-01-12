0f537e075745cd041eb7f39b559e0a5cc47a5b3b
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:46 -0300

    feat: add 31 variable scope

---
+++ 31_variableScope/index.js
@@ -0,0 +1,25 @@
+const verdadeira = true;
+/*
+let nome = 'João';
+var nome2 = 'Maria';
+
+if (verdadeira) {
+    let nome = 'Pedro';
+    console.log(nome, nome2);
+
+    if (verdadeira) {
+        let nome = 'Outra coisa';
+        console.log(nome, nome2);
+    }
+}*/
+
+function falaOi() {
+    var sobrenome = 'João';
+
+    if (verdadeira) {
+        let nome = 'Maria';
+        console.log(nome);
+    }
+    //console.log('Oi');
+}
+falaOi();
