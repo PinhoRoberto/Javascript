505ad0910b8845ad85f1a36d84ac53ef847e89c9
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:48 -0300

    feat: add 39 while

---
+++ 39_while/do_while/index.js
@@ -0,0 +1,36 @@
+/* let i = 0;
+while (i <=10) {
+    console.log (i);
+    i++;
+}
+
+console.log('segue a vida'); */
+/* const nome ='Luiz';
+
+let i = 0;
+while (i < nome.length) {
+    console.log (nome[i]);
+    i++;
+}
+
+console.log('segue a vida'); */
+
+function random (min, max) {
+    const r = Math.random() * (max - min) +min;
+    return Math.floor(r);
+}
+
+const min = 1;
+const max = 50;
+let rand  = random (min, max);
+
+while (rand !==10) {
+    rand = random (min, max);
+    console.log(rand);
+}
+console.log('segue a vida'); 
+
+do {rand = random (min, max);
+    console.log(rand);
+} while (rand !==10);
+console.log('checado do while');  
