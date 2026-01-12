b99ab29d500bfb7ac52fd2f8cf7ce4d093600989
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:44 -0300

    feat: add 14 number methods

---
+++ 14_numberMethods/index.js
@@ -0,0 +1,11 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 12.323232332;
+let num4 = 0.7;
+
+console.log (num1.toString() + num2); //converte num1 para string e concatena com num2
+console.log (typeof num1);
+console.log (num3.toFixed(2)); //converte num1 para binario
+console.log (Number.isInteger(num4)); //verifica se é inteiro
+
+//num1 = number(num1.toFixed(2)); //converte num1 para float com 2 casas decimais
