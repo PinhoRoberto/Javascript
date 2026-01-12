1fe2afd018abfdc3ace17257425142d26e76a274
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:48 -0300

    feat: add 40 break

---
+++ 40_break/continue/index.js
@@ -0,0 +1,28 @@
+/* const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let numero of numeros) {
+    if (numero === 2 || numero === 5) {
+        console.log('pulando o número', numero);
+        continue;
+    }
+    console.log(numero);
+
+if (numero === 7) {
+    console.log('7 encontrado, saindo...');
+    break;
+}
+} */
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (let i in numeros) {
+    let numero = numeros[i];
+    if (numero === 2 || numero === 5) {
+        console.log('pulando o número', numero);
+        continue;
+    }
+    console.log(numero);
+
+if (numero === 7) {
+    console.log('7 encontrado, saindo...');
+    break;
+}
+}
