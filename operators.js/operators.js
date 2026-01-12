3ce08e90521e66206d5fdb059110ff98b0d8fae4
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:51 -0300

    feat: add operators

---
+++ operators.js/operators.js
@@ -0,0 +1,19 @@
+let num = 20;
+
+num += 10;
+console.log(num);
+
+num -= 5;
+onsole.log(num);
+
+num *=2;
+console.log(num);
+
+num /= 10;
+console.log(num);
+
+num++;
+console.log(num);
+
+console.log(27 %2);
+
