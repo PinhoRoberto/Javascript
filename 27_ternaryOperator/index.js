77782d3b0331ef3fccc3ea990e780b4fe84e760b
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:45 -0300

    feat: add 27 ternary operator

---
+++ 27_ternaryOperator/index.js
@@ -0,0 +1,13 @@
+const pontuaçãousuario = 999;
+if (pontuaçãousuario >= 1000) {
+    console.log('Usuário VIP');
+}else {
+    console.log('Usuário Normal');
+}
+
+// const nivelUsuario = pontuaçãousuario >=1000 ? 'Usuário VIP' : 'Usuário Normal';
+// console.log(nivelUsuario);   
+
+//const corUsuario = null;
+//const corPadrao = corUsuario || 'Preto';
+//console.log(nivelUsuario, corPadrao);
