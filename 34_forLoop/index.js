437922f6923da250d9014eef7f6942bfa14dfc0b
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:47 -0300

    feat: add 34 for loop

---
+++ 34_forLoop/index.js
@@ -0,0 +1,20 @@
+/*for (let i = 0; i <=5 ; i++) {
+    console.log (`Llinha ${i}`);  
+}
+for (let i = 300; i <=500 ; i++) {
+   console.log (`Llinha ${i}`);  
+}
+
+for (let i = 100; i <=500 ; i+=10) {
+  console.log (`Llinha ${i}`);  
+}
+
+for (let i = 0; i <=10 ; i++) {
+    const par = i % 2 ===0;
+    console.log (i, par); 
+    } */
+
+const frutas = ['maçã', 'banana', 'pera', 'uva', 'laranja'];
+for (let i = 0; i <frutas.length; i++) {
+    console.log (`Indice ${i}`, frutas[i]
+    );}
