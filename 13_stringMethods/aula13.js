4aadffc1ab029fb66c794faca0eb342e14e7a21b
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:44 -0300

    feat: add 13 string methods

---
+++ 13_stringMethods/aula13.js
@@ -0,0 +1,6 @@
+let umaString = 'O rato roeu a roupa do rei de roma.';
+
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
+console.log(umaString.length);
+console.log(umaString[7 ]);
