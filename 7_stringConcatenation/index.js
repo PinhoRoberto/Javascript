e9899030a30675f229db3e6667abf514b62a90f2
Roberto Pinho <roberto.pinho@protonmail.com>
2026-01-12 14:33:49 -0300

    feat: add 7 string concatenation

---
+++ 7_stringConcatenation/index.js
@@ -0,0 +1,10 @@
+const nome = "Johnathan";
+console.log("Meu nome é " + nome + ", estou aprendendo Javascript"); 
+
+const primeiroNumero = 10;
+const segundoNumero = 20;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+console.log(resultado);
+console.log(resultadoDuplicado);
+console.log(typeof primeiroNumero);
